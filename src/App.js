6659881e17c9e36fe80da397bed8db224776dc8e
Peo2 <ajeigbefelix033@gmail.com>
2025-01-09 21:55:02 +0100

    new element

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Entry } from './Components/Main';
 function App() {
   return (
     <div className="bg-mainbg h-[844px] pt-20 lg:max-h-[640px] ">
-      <div className='  bg-white max-w-[310px] ml-9cd h-[400px]  mt-20 lg:max-w-[800px] lg:ml-64 lg:mt-10 lg:pt-12 lg:pl-48'>
+      <div className='  bg-white max-w-[310px] ml-9 h-[400px]  mt-20 lg:max-w-[800px] lg:ml-64 lg:mt-10 lg:pt-12 lg:pl-48'>
       <Head/>
       <Entry/>
       </div>
